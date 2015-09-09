e8c7f8b03fc899bce285550817ddc108496d8033
nilocy <mahaffec@gmail.com>
2015-09-09 11:36:39 -0400

    updates pics to date 7

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-activerecord'
+gem 'rake'
+gem 'pg'
--- app.rb
+++ app.rb
@@ -4,3 +4,27 @@ Bundler.require
 get '/' do
   erb :index
 end
+
+get '/1' do
+  erb :show1
+end
+
+get '/2' do
+  erb :show2
+end
+get '/3' do
+  erb :show3
+end
+get '/4' do
+  erb :show4
+end
+get '/5' do
+  erb :show5
+end
+
+get '/6' do
+  erb :show6
+end
+get '/7' do
+  erb :show7
+end
